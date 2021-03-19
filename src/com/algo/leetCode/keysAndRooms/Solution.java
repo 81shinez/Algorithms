42ae8ae8051a13f1759d144dfebf77bd5d6ae71a
81Shinez <despiguile@gmail.com>
2021-03-20 00:31:20 +0900

    20210320 LeetCode Keys and Rooms

---
+++ src/com/algo/leetCode/keysAndRooms/Solution.java
@@ -0,0 +1,27 @@
+package com.algo.leetCode.keysAndRooms;
+
+import java.util.*;
+
+class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        LinkedList<Integer> openList = new LinkedList<>();
+        HashSet<Integer> isOpened = new HashSet<>();
+
+        isOpened.add(0);
+        openList.add(0);
+
+        while (!openList.isEmpty()){
+            int size = openList.size();
+            for(int i = 0; i < size; i++){
+                for(int room : rooms.get(openList.pollFirst())){
+                    if(!isOpened.contains(room)){
+                        openList.add(room);
+                        isOpened.add(room);
+                    }
+                }
+            }
+        }
+
+        return isOpened.size() == rooms.size() ? true : false;
+    }
+}
